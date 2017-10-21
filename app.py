49aa4776231baca429af67b25f60dbde59d0bc51
loganmurphy <loganmurphy1984@gmail.com>
2017-10-20 21:15:28 -0500

    updated the redirect url

--- app.py
+++ app.py
@@ -81,6 +81,7 @@ class GoogleOAuth2LoginHandler(tornado.web.RequestHandler,
             print('Cookie set!')
             self.redirect('page/profile.html', {})
 
+
         else:
             yield self.authorize_redirect(
                 redirect_uri='http://localhost:mind-cloud.logancodes.com/auth',
