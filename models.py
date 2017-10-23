1e380d5adba96ad022ac50c8e777d3927ad76e77
loganmurphy <loganmurphy1984@gmail.com>
2017-10-22 23:30:47 -0500

    updated site for deployment

--- models.py
+++ models.py
@@ -33,9 +33,8 @@ class Goals (BaseModel):
   person = peewee.ForeignKeyField(Person, null=True)
   title = peewee.CharField(max_length=60)
   achievement = peewee.TextField()
-  slug = peewee.CharField(max_length=50, unique=True)
   reminder = peewee.DateTimeField(
             default=datetime.datetime.utcnow)
 
   def __str__ (self):
-    return self.Person
+    return self.Person, self.title
